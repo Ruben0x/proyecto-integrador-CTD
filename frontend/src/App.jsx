f9168a6f53b8e1a3077a1c5aa42877cb3f37e467
claudioace <claudioace@gmail.com>
2024-05-07 21:29:30 -0400

    version primitiva del body

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,13 @@ import { CreateNewUserPage } from './pages/CreateNewUserPage';
 import { HomePage } from './pages/HomePage';
 import { LogInPage } from './pages/LogInPage';
 import ResponsiveAppBar from './components/ResponsiveAppBar';
+import ResponsiveBody from './components/ResponsiveBody';
 
 export const App = () => {
   return (
     <>
       <ResponsiveAppBar />
+      <ResponsiveBody />
       <Routes>
         <Route element={<LogInPage />} path='/login' />
         <Route element={<CreateNewUserPage />} path='/createuser' />
