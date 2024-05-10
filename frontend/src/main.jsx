a77e86494289781df464671d715b56bc12319ba5
ejmorales <ejmorales.seiza@bancochile.cl>
2024-05-10 19:52:20 -0400

    paleta de colores globales

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App.jsx';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ff5000"
+    },
+      secondary: {
+      main: "#121312"
+    },
+    terceario: {
+      main: "#000000"
+    },
+    neutralColor: {
+      main: "#ffffff"
+    }
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
+      <ThemeProvider theme={theme}>
       <App />
+      </ThemeProvider>
     </BrowserRouter>
 );
