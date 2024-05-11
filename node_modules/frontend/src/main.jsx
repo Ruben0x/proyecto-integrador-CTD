c42860b190cb414c08b6801a17539b3eabcc6f8c
ruben <r.alejandro.gn@gmail.com>
2024-05-10 21:31:13 -0400

    Correccion merge anterior con errores

--- node_modules/frontend/src/main.jsx
+++ node_modules/frontend/src/main.jsx
@@ -3,9 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App.jsx';
+import { createTheme } from '@mui/material';
+import { ThemeProvider } from '@emotion/react';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ff5000',
+    },
+    secondary: {
+      main: '#121312',
+    },
+    terceario: {
+      main: '#000000',
+    },
+    neutralColor: {
+      main: '#ffffff',
+    },
+  },
+});
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
       <App />
-    </BrowserRouter>
+    </ThemeProvider>
+  </BrowserRouter>
 );
