a9b1d1e942e6456823502a3cfd09d2c87062383f
JoserynE <Joseryncaraballo@hotmail.com>
2024-05-10 20:28:17 -0400

    Agregar footer

--- node_modules/frontend/src/App.jsx
+++ node_modules/frontend/src/App.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { CreateNewUserPage } from './pages/CreateNewUserPage';
-import { HomePage } from './pages/HomePage';
-import { LogInPage } from './pages/LogInPage';
-import ResponsiveAppBar from './components/ResponsiveAppBar';
-import ResponsiveBody from './components/ResponsiveBody';
-import { AddProductPage } from './pages/AddProductPage';
-
-import { AdminPage } from './pages/AdminPage';
+import { Route, Routes } from "react-router-dom";
+import { CreateNewUserPage } from "./pages/CreateNewUserPage";
+import { HomePage } from "./pages/HomePage";
+import { LogInPage } from "./pages/LogInPage";
+import ResponsiveAppBar from "./components/ResponsiveAppBar";
+import { AddProductPage } from "./pages/AddProductPage";
+import { AdminPage } from "./pages/AdminPage";
+import { Footer } from "./components/Footer";
 
 export const App = () => {
   return (
     <>
       <ResponsiveAppBar />
       <Routes>
-        <Route element={<LogInPage />} path='/login' />
-        <Route element={<CreateNewUserPage />} path='/createuser' />
-        <Route element={<HomePage />} path='/' />
-        <Route element={<AddProductPage />} path='/addProduct' />
-        <Route element={<AdminPage />} path='/administrador' />
+        <Route element={<LogInPage />} path="/login" />
+        <Route element={<CreateNewUserPage />} path="/createuser" />
+        <Route element={<HomePage />} path="/" />
+        <Route element={<AddProductPage />} path="/addProduct" />
+        <Route element={<AdminPage />} path="/administrador" />
       </Routes>
+
+      <Footer />
     </>
   );
 };
