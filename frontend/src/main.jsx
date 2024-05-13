c9a64b38c3a0a2fc5e142b5b6c7c5dda340c22ea
ejmorales <ejmorales.seiza@bancochile.cl>
2024-05-13 00:54:14 -0400

    aceptando cambios

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,17 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App.jsx';
-<<<<<<< HEAD
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-=======
-import { createTheme } from '@mui/material';
-import { ThemeProvider } from '@emotion/react';
->>>>>>> 892336f64ac339b39b90636959f45b6a47e97d18
 
 const theme = createTheme({
   palette: {
     primary: {
-<<<<<<< HEAD
       main: "#ff5000" // orange
     },
       secondary: {
@@ -26,20 +20,6 @@ const theme = createTheme({
       main: "#ffffff" // white
     }
   }
-=======
-      main: '#ff5000',
-    },
-    secondary: {
-      main: '#121312',
-    },
-    terceario: {
-      main: '#000000',
-    },
-    neutralColor: {
-      main: '#ffffff',
-    },
-  },
->>>>>>> 892336f64ac339b39b90636959f45b6a47e97d18
 });
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
