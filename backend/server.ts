17eab6a5e19fb63663c55dfb87bf284f1b4edb42
Ruben Gonzalez <r.alejandro.gn@gmail.com>
2024-05-13 12:31:00 -0400

    Revert "cambios a navBar"

---
+++ backend/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = process.env.PORT ?? 3000;
+
+app.use(cors()); //Enable CORS
+
+app.get('/', (req, res) => {
+  return res.status(200).json({ data: [], message: 'Hola mundo!' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
