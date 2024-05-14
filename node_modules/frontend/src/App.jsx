db153e89da1c75bc421f666a2ad4606736529fb2
ruben <r.alejandro.gn@gmail.com>
2024-05-14 00:23:01 -0400

    Arreglo de rutas en App.jsx

--- node_modules/frontend/src/App.jsx
+++ node_modules/frontend/src/App.jsx
@@ -1,30 +1,16 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { CreateNewUserPage } from './pages/CreateNewUserPage';
-import { HomePage } from './pages/HomePage';
-import { LogInPage } from './pages/LogInPage';
-import ResponsiveAppBar from './components/ResponsiveAppBar';
-import ResponsiveBody from './components/ResponsiveBody';
-import { AddProductPage } from './pages/AddProductPage';
-
-import { AdminPage } from './pages/AdminPage';
-import { AdminProductList } from './pages/AdminProductList';
 import { Footer } from './components/Footer';
+import { TiendaRouter } from './routes/TiendaRouter';
+import { AdminPage } from './pages/AdminPage';
 
 export const App = () => {
   return (
     <>
-      <ResponsiveAppBar />
       <Routes>
-        <Route element={<HomePage />} path='/' />
-        <Route element={<LogInPage />} path='/login' />
-        <Route element={<CreateNewUserPage />} path='/createuser' />
-        <Route element={<AdminPage />} path='/administracion' />
-        <Route element={<AdminProductList />} path='/listar-productos' />
-        <Route element={<AddProductPage />} path='/agregar-productos' />
+        <Route path='/administracion' element={<AdminPage />} />
+        <Route path='/*' element={<TiendaRouter />} />
       </Routes>
-
-      <Footer />
     </>
   );
 };
