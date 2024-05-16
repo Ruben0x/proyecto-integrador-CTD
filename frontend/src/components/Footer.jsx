ee4fe85afd4f9bab7489bb1bcd8484762530b3d6
ejmorales <ejmorales.seiza@bancochile.cl>
2024-05-15 20:36:06 -0400

    elaboración del footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,3 +1,52 @@
+import React from "react";
+
+import LOGO from "../assets/img/LOGO.svg";
+import { Link } from "react-router-dom";
+import facebook from "../assets/img/socialMediaIcons/facebook.png";
+import tiktok from "../assets/img/socialMediaIcons/tiktok.png";
+import whatsapp from "../assets/img/socialMediaIcons/whatsapp.png";
+import instagram from "../assets/img/socialMediaIcons/instagram.png";
+import youtube from "../assets/img/socialMediaIcons/youtube.png";
+import "./Footer.css";
+
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="main-footer">
+    <div className="footer-logo">
+      {/* columna 1 - logo */}
+      <Link to={"/"} style={{ textDecoration: "none" }}>
+        <img src={LOGO} alt="" />
+      </Link>
+    </div>
+
+    <div className="footer-copyright">
+      <p>
+        {" "}
+        {/* columna 2 - titulo */}
+        &copy;{new Date().getFullYear()}&nbsp;-&nbsp;PORTAL&nbsp;SONORO
+      </p>
+    </div>
+
+    <div className="footer-social-media">
+      {/* columna 3 - iconos */}
+      <ul className="Lista-sin-estilo">
+        <li>
+          <img src={facebook} alt="" />
+        </li>
+        <li>
+          <img src={instagram} alt="" />
+        </li>
+        <li>
+          <img src={tiktok} alt="" />
+        </li>
+        <li>
+          <img src={youtube} alt="" />
+        </li>
+        <li>
+          <img src={whatsapp} alt="" />
+        </li>
+      </ul>
+    </div>
+  </div>
+  );
 };
