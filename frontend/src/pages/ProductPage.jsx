5481af68b25b2d4f04a1c569df1be009ea7733e9
ejmorales <ejmorales.seiza@bancochile.cl>
2024-06-05 00:34:08 -0400

    primeros avances politicas

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,10 +1,10 @@
-import { Box, Container, Link, Typography } from '@mui/material';
-import React, { useEffect, useState } from 'react';
-import { GridImagenes } from '../components/GridImagenes';
-import WestIcon from '@mui/icons-material/West';
-import { Link as RouterLink, Navigate, useParams } from 'react-router-dom';
-import axios from 'axios';
-import { Characteristics } from '../products/components/Characteristics';
+import { Box, Container, Link, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { GridImagenes } from "../components/GridImagenes";
+import WestIcon from "@mui/icons-material/West";
+import { Link as RouterLink, Navigate, useParams } from "react-router-dom";
+import axios from "axios";
+import { Characteristics } from "../products/components/Characteristics";
 
 export const ProductPage = () => {
   const { id } = useParams();
@@ -12,7 +12,7 @@ export const ProductPage = () => {
   const [listaImagenes, setListaImagenes] = useState([]);
 
   useEffect(() => {
-    axios('http://localhost:3000/productos/' + id)
+    axios("http://localhost:3000/productos/" + id)
       .then((res) => {
         setInstrumento(res.data);
         setListaImagenes(res.data.imagenes || []);
@@ -23,52 +23,52 @@ export const ProductPage = () => {
   }, [id]);
 
   if (!instrumento) {
-    return <Navigate to={'/'} />;
+    return <Navigate to={"/"} />;
   }
 
   // console.log(instrumento);
 
   return (
-    <Container sx={{ minHeight: '90vh', backgroundColor: 'white' }}>
+    <Container sx={{ minHeight: "90vh", backgroundColor: "white" }}>
       <Box sx={{ margin: 2 }}>
         <Box paddingY={2}>
           <Link
             component={RouterLink}
-            style={{ textDecoration: 'none' }}
-            to={'/'}
+            style={{ textDecoration: "none" }}
+            to={"/"}
           >
-            <Typography sx={{ display: 'flex', alignItems: 'center' }}>
-              <WestIcon fontSize='large' sx={{ paddingRight: 2 }} />
+            <Typography sx={{ display: "flex", alignItems: "center" }}>
+              <WestIcon fontSize="large" sx={{ paddingRight: 2 }} />
               VOLVER AL HOME
             </Typography>
           </Link>
         </Box>
         <Typography
-          variant='h3'
+          variant="h3"
           sx={{
-            color: '#ff5000',
-            fontWeight: '600',
-            textTransform: 'uppercase',
+            color: "#ff5000",
+            fontWeight: "600",
+            textTransform: "uppercase",
           }}
         >
           {instrumento.nombreMarca}
-          <span style={{ color: '#000000' }}> {instrumento.nombre}</span>
+          <span style={{ color: "#000000" }}> {instrumento.nombre}</span>
         </Typography>
         <GridImagenes listaImagenes={listaImagenes} />
-        <Box sx={{ padding: 2, backgroundColor: '#000000', color: 'white' }}>
-          <Typography variant='h4' color='white'>
+        <Box sx={{ padding: 2, backgroundColor: "#000000", color: "white" }}>
+          <Typography variant="h4" color="white">
             {instrumento.nombreCategoria}
           </Typography>
-          <Typography variant='h4' color='primary'>
-            {' '}
-            {instrumento.nombreMarca}{' '}
+          <Typography variant="h4" color="primary">
+            {" "}
+            {instrumento.nombreMarca}{" "}
           </Typography>
-          <Typography variant='subtitle1' py={1}>
+          <Typography variant="subtitle1" py={1}>
             {instrumento.descripcion}
           </Typography>
-          <Typography variant='h5' color='primary' py={1}>
-            ${instrumento.precio}{' '}
-            <span style={{ color: 'white' }}>/diario</span>
+          <Typography variant="h5" color="primary" py={1}>
+            ${instrumento.precio}{" "}
+            <span style={{ color: "white" }}>/diario</span>
           </Typography>
         </Box>
         <Characteristics instrumento={instrumento} />
---
+++ frontend/src/products/components/Politicas.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Politicas() {
+  return (
+    <div>
+      <h4>POLÍTICAS DE RESERVA</h4>
+      <ul>
+        <li>El instrumento debe ser devuelto enel mismo estado en que fue entregado</li>
+        <li>El arriendo del instrumento es válido hasta las 18:00 Hrs del último día de reserva. Después de esto, se aplicará una multa del doble del valor diario por cada día de atraso</li>
+        <li>El instrumento comprometido en la reserva es de uso exclusivo del usuario registrado que realiza la reserva, no se aceptarán situaciones atribuibles a terceros.</li>
+        <li>Es obligación y responsabilidad exclusiva del usuario registrado que raliza la reserva, el hacer que las políticas antes mencionadas sean cumplidas. Cualquier situación que afecte a lo anterior, puede estar sujeta a multas por parte de PORTAL SONORO</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Politicas
