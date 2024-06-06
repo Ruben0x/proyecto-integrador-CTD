370b509095f404c7c324922ad34a63fa388f3982
Jose Triviño <josetrivino@gmail.com>
2024-06-06 12:53:29 -0400

    actualiza favs, link de producto y lighbox de imagenes

--- frontend/src/auth/components/TableAllFavs.jsx
+++ frontend/src/auth/components/TableAllFavs.jsx
@@ -1,6 +1,174 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { useFavoritos } from '../../context/store/FavoritosProvider';
-import { GlobalUserDataContext } from '../helpers/globalUserData';
+// import React, { useContext, useEffect, useState } from 'react';
+// import { useFavoritos } from '../../context/store/FavoritosProvider';
+// import { GlobalUserDataContext } from '../helpers/globalUserData';
+// import {
+//   Box,
+//   Button,
+//   Container,
+//   Dialog,
+//   DialogActions,
+//   DialogContent,
+//   DialogContentText,
+//   DialogTitle,
+//   Typography,
+// } from '@mui/material';
+// import HistoryIcon from '@mui/icons-material/History';
+// import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+// import ErrorOutlineOutlinedIcon from '@mui/icons-material/ErrorOutlineOutlined';
+// import { DataGrid } from '@mui/x-data-grid';
+
+// export const TableAllFavs = () => {
+//   const [deleteModal, setDeleteModal] = useState(false);
+//   const [favorito, setFavorito] = useState();
+
+//   const { getAllFavoritos, isLoading, favState, deleteFavs } = useFavoritos();
+//   const { globalUserData } = useContext(GlobalUserDataContext);
+
+//   useEffect(() => {
+//     getAllFavoritos(globalUserData.id);
+//   }, []);
+
+//   const handleClickOpen = (params) => {
+//     setFavorito(params.row);
+//     setDeleteModal(true);
+//   };
+//   const handleClose = () => {
+//     setDeleteModal(false);
+//   };
+//   if (isLoading) return 'Cargando...';
+
+//   const listaProductos = [];
+
+//   favState.favoritos.forEach((element) => {
+//     listaProductos.push(element.producto);
+//   });
+//   console.log(listaProductos);
+
+//   const handleAcceptDelete = (user, producto) => {
+//     deleteFavs(user, producto);
+//     setDeleteModal(false);
+//   };
+//   const renderActions = (params) => {
+//     return (
+//       <>
+//         <Button
+//           size='small'
+//           variant='contained'
+//           color='buttonRed'
+//           onClick={() => handleClickOpen(params)}
+//           startIcon={<DeleteOutlineOutlinedIcon />}
+//         >
+//           <Typography fontWeight={600}>Eliminar</Typography>
+//         </Button>
+//       </>
+//     );
+//   };
+
+//   return (
+//     <>
+//       <Container sx={{ display: 'flex', justifyContent: 'center' }}>
+//         <Box sx={{ width: '100%' }}>
+//           <DataGrid
+//             rows={listaProductos}
+//             columns={[
+//               {
+//                 field: 'imagen',
+//                 headerName: 'Imagen',
+//                 width: 100,
+//                 renderCell: (params) => {
+//                   const imageUrl = params.row.imagenUrl;
+//                   return (
+//                     <img
+//                       src={imageUrl}
+//                       alt={params.row.nombre}
+//                       style={{
+//                         width: '50px',
+//                         height: '50px',
+//                         objectFit: 'cover',
+//                       }}
+//                     />
+//                   );
+//                 },
+//               },
+//               { field: 'nombre', headerName: 'Nombre', width: 100 },
+//               {
+//                 field: 'categoriaS',
+//                 headerName: 'Categoria',
+//                 width: 100,
+//                 valueGetter: (values, row) => {
+//                   return `${row.categoriaS.nombre || ''}`;
+//                 },
+//               },
+//               {
+//                 field: 'marcaS',
+//                 headerName: 'Marca',
+//                 width: 100,
+//                 valueGetter: (values, row) => {
+//                   return `${row.marcaS.nombre || ''}`;
+//                 },
+//               },
+
+//               {
+//                 field: 'accions',
+//                 headerName: 'Acciones',
+//                 width: 100,
+//                 renderCell: renderActions,
+//               },
+//             ]}
+//             initialState={{
+//               pagination: {
+//                 paginationModel: { page: 0, pageSize: 5 },
+//               },
+//             }}
+//             pageSizeOptions={[5, 10]}
+//           />
+//         </Box>
+//       </Container>
+//       {deleteModal && (
+//         <Dialog
+//           open={deleteModal}
+//           onClose={handleClose}
+//           aria-labelledby='alert-dialog-title'
+//           aria-describedby='alert-dialog-description'
+//           fullWidth={true}
+//           maxWidth={'xs'}
+//           sx={{ textAlign: 'center' }}
+//         >
+//           <DialogContent>
+//             <ErrorOutlineOutlinedIcon sx={{ fontSize: 150 }} color='primary' />
+//           </DialogContent>
+//           <DialogTitle id='alert-dialog-title' fontWeight={600}>
+//             {'¿Estas seguro?'}
+//           </DialogTitle>
+//           <DialogContent>
+//             <DialogContentText id='alert-dialog-description' fontWeight={600}>
+//               Esta acción eliminara al producto de sus Favoritos
+//             </DialogContentText>
+//           </DialogContent>
+//           <DialogActions sx={{ justifyContent: 'center' }}>
+//             <Button
+//               variant='contained'
+//               color='buttonGreen'
+//               onClick={() => {
+//                 handleAcceptDelete(globalUserData.id, favorito.id);
+//               }}
+//               autoFocus
+//             >
+//               ELIMINAR
+//             </Button>
+//             <Button variant='contained' color='buttonRed' onClick={handleClose}>
+//               CANCELAR
+//             </Button>
+//           </DialogActions>
+//         </Dialog>
+//       )}
+//     </>
+//   );
+// };
+
+import React, { useContext, useEffect, useState } from "react";
+import { useFavoritos } from "../../context/store/FavoritosProvider";
+import { GlobalUserDataContext } from "../helpers/globalUserData";
 import {
   Box,
   Button,
@@ -10,12 +178,16 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
   Typography,
-} from '@mui/material';
-import HistoryIcon from '@mui/icons-material/History';
-import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
-import ErrorOutlineOutlinedIcon from '@mui/icons-material/ErrorOutlineOutlined';
-import { DataGrid } from '@mui/x-data-grid';
+} from "@mui/material";
+import Avatar from "@mui/material/Avatar";
+import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
+import ErrorOutlineOutlinedIcon from "@mui/icons-material/ErrorOutlineOutlined";
+import { Link } from "react-router-dom";
 
 export const TableAllFavs = () => {
   const [deleteModal, setDeleteModal] = useState(false);
@@ -26,137 +198,110 @@ export const TableAllFavs = () => {
 
   useEffect(() => {
     getAllFavoritos(globalUserData.id);
-  }, []);
+  }, [getAllFavoritos, globalUserData.id]);
 
-  const handleClickOpen = (params) => {
-    setFavorito(params.row);
+  const handleClickOpen = (favorito) => {
+    setFavorito(favorito);
     setDeleteModal(true);
   };
+
   const handleClose = () => {
     setDeleteModal(false);
   };
-  if (isLoading) return 'Cargando...';
 
-  const listaProductos = [];
-
-  favState.favoritos.forEach((element) => {
-    listaProductos.push(element.producto);
-  });
-  console.log(listaProductos);
+  if (isLoading) return "Cargando...";
 
   const handleAcceptDelete = (user, producto) => {
     deleteFavs(user, producto);
     setDeleteModal(false);
   };
-  const renderActions = (params) => {
-    return (
-      <>
-        <Button
-          size='small'
-          variant='contained'
-          color='buttonRed'
-          onClick={() => handleClickOpen(params)}
-          startIcon={<DeleteOutlineOutlinedIcon />}
-        >
-          <Typography fontWeight={600}>Eliminar</Typography>
-        </Button>
-      </>
-    );
-  };
 
   return (
     <>
-      <Container sx={{ display: 'flex', justifyContent: 'center' }}>
-        <Box sx={{ width: '100%' }}>
-          <DataGrid
-            rows={listaProductos}
-            columns={[
-              {
-                field: 'imagen',
-                headerName: 'Imagen',
-                width: 100,
-                renderCell: (params) => {
-                  const imageUrl = params.row.imagenUrl;
-                  return (
-                    <img
-                      src={imageUrl}
-                      alt={params.row.nombre}
-                      style={{
-                        width: '50px',
-                        height: '50px',
-                        objectFit: 'cover',
-                      }}
-                    />
-                  );
-                },
-              },
-              { field: 'nombre', headerName: 'Nombre', width: 100 },
-              {
-                field: 'categoriaS',
-                headerName: 'Categoria',
-                width: 100,
-                valueGetter: (values, row) => {
-                  return `${row.categoriaS.nombre || ''}`;
-                },
-              },
-              {
-                field: 'marcaS',
-                headerName: 'Marca',
-                width: 100,
-                valueGetter: (values, row) => {
-                  return `${row.marcaS.nombre || ''}`;
-                },
-              },
-
-              {
-                field: 'accions',
-                headerName: 'Acciones',
-                width: 100,
-                renderCell: renderActions,
-              },
-            ]}
-            initialState={{
-              pagination: {
-                paginationModel: { page: 0, pageSize: 5 },
-              },
-            }}
-            pageSizeOptions={[5, 10]}
-          />
+      <Container
+        sx={{ display: "flex", justifyContent: "center", marginTop: 2 }}
+      >
+        <Box sx={{ width: "100%" }}>
+          <List>
+            {favState.favoritos.map((fav) => (
+              <ListItem
+                key={fav.producto.id}
+                sx={{ display: "flex", alignItems: "center" }}
+              >
+                <ListItemAvatar>
+                  <Avatar
+                    src={fav.producto.imagenUrl}
+                    alt={fav.producto.nombre}
+                    sx={{ width: 50, height: 50 }}
+                  />
+                </ListItemAvatar>
+                <ListItemText
+                  primary={
+                    <Link
+                      to={`/instrumentos/${fav.producto.id}`}
+                      style={{ textDecoration: "none", color: "inherit" }}
+                    >
+                      <Typography
+                        variant="body1"
+                        component="span"
+                        fontWeight="bold"
+                      >
+                        {fav.producto.nombre}
+                      </Typography>
+                    </Link>
+                  }
+                  secondary={
+                    <>
+                      <Typography variant="body2">{`Categoría: ${fav.producto.categoriaS.nombre}`}</Typography>
+                      <Typography variant="body2">{`Marca: ${fav.producto.marcaS.nombre}`}</Typography>
+                    </>
+                  }
+                />
+                <Button
+                  size="small"
+                  variant="contained"
+                  color="buttonRed"
+                  onClick={() => handleClickOpen(fav.producto)}
+                  startIcon={<DeleteOutlineOutlinedIcon />}
+                >
+                  <Typography fontWeight={600}>Eliminar</Typography>
+                </Button>
+              </ListItem>
+            ))}
+          </List>
         </Box>
       </Container>
       {deleteModal && (
         <Dialog
           open={deleteModal}
           onClose={handleClose}
-          aria-labelledby='alert-dialog-title'
-          aria-describedby='alert-dialog-description'
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
           fullWidth={true}
-          maxWidth={'xs'}
-          sx={{ textAlign: 'center' }}
+          maxWidth={"xs"}
+          sx={{ textAlign: "center" }}
         >
           <DialogContent>
-            <ErrorOutlineOutlinedIcon sx={{ fontSize: 150 }} color='primary' />
+            <ErrorOutlineOutlinedIcon sx={{ fontSize: 150 }} color="primary" />
           </DialogContent>
-          <DialogTitle id='alert-dialog-title' fontWeight={600}>
-            {'¿Estas seguro?'}
+          <DialogTitle id="alert-dialog-title" fontWeight={600}>
+            {"¿Estas seguro?"}
           </DialogTitle>
           <DialogContent>
-            <DialogContentText id='alert-dialog-description' fontWeight={600}>
-              Esta acción eliminara al producto de sus Favoritos
+            <DialogContentText id="alert-dialog-description" fontWeight={600}>
+              Esta acción eliminará al producto de sus Favoritos
             </DialogContentText>
           </DialogContent>
-          <DialogActions sx={{ justifyContent: 'center' }}>
+          <DialogActions sx={{ justifyContent: "center" }}>
             <Button
-              variant='contained'
-              color='buttonGreen'
-              onClick={() => {
-                handleAcceptDelete(globalUserData.id, favorito.id);
-              }}
+              variant="contained"
+              color="buttonGreen"
+              onClick={() => handleAcceptDelete(globalUserData.id, favorito.id)}
               autoFocus
             >
               ELIMINAR
             </Button>
-            <Button variant='contained' color='buttonRed' onClick={handleClose}>
+            <Button variant="contained" color="buttonRed" onClick={handleClose}>
               CANCELAR
             </Button>
           </DialogActions>
--- frontend/src/components/GridInstrumentos.jsx
+++ frontend/src/components/GridInstrumentos.jsx
@@ -1,7 +1,48 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { ItemsContext } from '../context/ItemsContext';
-import { Box, Grid } from '@mui/material';
-import { InstrumentCardResponsive } from './InstrumentCardResponsive';
+// import React, { useContext, useEffect, useState } from 'react';
+// import { ItemsContext } from '../context/ItemsContext';
+// import { Box, Grid } from '@mui/material';
+// import { InstrumentCardResponsive } from './InstrumentCardResponsive';
+
+// export const GridInstrumentos = () => {
+//   const { itemState } = useContext(ItemsContext);
+//   const [productos, setProductos] = useState([]);
+
+//   useEffect(() => {
+//     if (itemState.itemsRandoms) {
+//       setProductos(itemState.itemsRandoms);
+//     }
+//   }, [itemState]);
+//   return (
+//     <Box
+//       pt={4}
+//       sx={{
+//         flexGrow: 1,
+//       }}
+//     >
+//       <Grid
+//         container
+//         spacing={{ xs: 2, md: 3 }}
+//         columns={{ xs: 4, sm: 8, md: 12 }}
+//         justifyContent={'center'}
+//       >
+//         {productos.map((instrument) => (
+//           <Grid item xs={4} sm={6} md={6} key={instrument.id}>
+//             <InstrumentCardResponsive
+//               key={instrument.id}
+//               instrument={instrument}
+//             />
+//           </Grid>
+//         ))}
+//       </Grid>
+//     </Box>
+//   );
+// };
+
+import React, { useContext, useEffect, useState } from "react";
+import { ItemsContext } from "../context/ItemsContext";
+import { Box, Grid } from "@mui/material";
+import { InstrumentCardResponsive } from "./InstrumentCardResponsive";
+import { Link } from "react-router-dom";
 
 export const GridInstrumentos = () => {
   const { itemState } = useContext(ItemsContext);
@@ -12,6 +53,7 @@ export const GridInstrumentos = () => {
       setProductos(itemState.itemsRandoms);
     }
   }, [itemState]);
+
   return (
     <Box
       pt={4}
@@ -23,14 +65,19 @@ export const GridInstrumentos = () => {
         container
         spacing={{ xs: 2, md: 3 }}
         columns={{ xs: 4, sm: 8, md: 12 }}
-        justifyContent={'center'}
+        justifyContent={"center"}
       >
         {productos.map((instrument) => (
           <Grid item xs={4} sm={6} md={6} key={instrument.id}>
-            <InstrumentCardResponsive
-              key={instrument.id}
-              instrument={instrument}
-            />
+            <Link
+              to={`/instrumentos/${instrument.id}`}
+              style={{ textDecoration: "none" }}
+            >
+              <InstrumentCardResponsive
+                key={instrument.id}
+                instrument={instrument}
+              />
+            </Link>
           </Grid>
         ))}
       </Grid>
