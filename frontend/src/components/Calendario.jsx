e257d107022d19f85c8f3e8d8636a61393d0988e
ruben <r.alejandro.gn@gmail.com>
2024-06-05 23:53:26 -0400

    Pagina de producto: Corregido posicion de politicas y estilo de calendario

--- frontend/src/components/Calendario.jsx
+++ frontend/src/components/Calendario.jsx
@@ -1,21 +1,39 @@
 //https://www.npmjs.com/package/react-multi-date-picker
 
 import '../styles/calendar.css';
-import React, { useState } from "react";
-import DatePicker, { DateObject } from "react-multi-date-picker";
-import "react-multi-date-picker/styles/backgrounds/bg-dark.css"
+import React, { useState } from 'react';
+import DatePicker, { DateObject } from 'react-multi-date-picker';
+import 'react-multi-date-picker/styles/backgrounds/bg-dark.css';
 
-const dias = [['Domingo', 'Do'], ['Lunes', 'Lu'], ['Martes', 'Ma'], ['Miércoles', 'Mi'], ['Jueves', 'Ju'], ['Viernes', 'Vi'], ['Sábado', 'Sá']]
-const meses = [['Enero', 'Ene'], ['Febrero', 'Feb'], ['Marzo', 'Mar'], ['Abril', 'Abr'], ['Mayo', 'May'], ['Junio', 'Jun'], ['Julio', 'Jul'], ['Agosto', 'Ago'], ['Septiembre', 'Sep'], ['Octubre', 'Oct'], ['Noviembre', 'Nov'], ['Diciembre', 'Dic']
-]
+const dias = [
+  ['Domingo', 'Do'],
+  ['Lunes', 'Lu'],
+  ['Martes', 'Ma'],
+  ['Miércoles', 'Mi'],
+  ['Jueves', 'Ju'],
+  ['Viernes', 'Vi'],
+  ['Sábado', 'Sá'],
+];
+const meses = [
+  ['Enero', 'Ene'],
+  ['Febrero', 'Feb'],
+  ['Marzo', 'Mar'],
+  ['Abril', 'Abr'],
+  ['Mayo', 'May'],
+  ['Junio', 'Jun'],
+  ['Julio', 'Jul'],
+  ['Agosto', 'Ago'],
+  ['Septiembre', 'Sep'],
+  ['Octubre', 'Oct'],
+  ['Noviembre', 'Nov'],
+  ['Diciembre', 'Dic'],
+];
 
 export const Calendario = () => {
-    const [values, setValues] = useState([,])
-    return (
-
-      <DatePicker
-      className="bg-dark orange"
-      
+  const [values, setValues] = useState([,]);
+  return (
+    <DatePicker
+      className='bg-dark orange'
       value={values}
       onChange={setValues}
       range
@@ -23,25 +41,23 @@ export const Calendario = () => {
       weekStartDayIndex={1}
       weekDays={dias}
       months={meses}
-      format="DD/MMM/YYYY"
+      format='DD/MMM/YYYY'
       minDate={new DateObject()}
-      placeholder="Disponibilidad de fechas"
+      placeholder='Disponibilidad de fechas'
       style={{
-        width: "100%",
+        width: '100%',
         backgroundColor: 'neutralColor',
-        height: "24px",
-        borderRadius: "4px",
-        fontSize: "14px",
-        padding: "4px 10px",
+        height: '24px',
+        borderRadius: '4px',
+        fontSize: '14px',
+        padding: '4px 10px',
         border: 0,
-        margin:0,
-        minHeight:'40px',
+        margin: 0,
+        minHeight: '40px',
       }}
       containerStyle={{
-        width: "100%"
+        width: '100%',
       }}
-      
-      />   
-
+    />
   );
 };
