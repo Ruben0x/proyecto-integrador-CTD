bdbf1e25037f5c530528d8fe26a13de855722f68
Jose Triviño <josetrivino@gmail.com>
2024-06-11 19:53:25 -0400

    update repo

--- frontend/src/routes/TiendaRouter.jsx
+++ frontend/src/routes/TiendaRouter.jsx
@@ -1,17 +1,17 @@
-import React, { useContext, useEffect } from 'react';
-import ResponsiveAppBar from '../components/ResponsiveAppBar';
-import { Route, Routes } from 'react-router-dom';
-import { HomePage } from '../pages/HomePage';
-import { Footer } from '../components/Footer';
-import { RegisterPage } from '../auth/RegisterPage';
-import { LogInPage } from '../auth/LogInPage';
-import { UserInfoPage } from '../auth/UserInfoPage';
-import { ProductPage } from '../products/pages/ProductPage';
-import { ProductCatPage } from '../pages/ProductCatPage';
-import { useUsers } from '../context/store/UsersProvider';
-import { GlobalUserDataContext } from '../auth/helpers/globalUserData';
-import { ProductSearchPage } from '../pages/ProductSearchPage';
-import { Toaster } from 'sonner';
+import React, { useContext, useEffect } from "react";
+import ResponsiveAppBar from "../components/ResponsiveAppBar";
+import { Route, Routes } from "react-router-dom";
+import { HomePage } from "../pages/HomePage";
+import { Footer } from "../components/Footer";
+import { RegisterPage } from "../auth/RegisterPage";
+import { LogInPage } from "../auth/LogInPage";
+import { UserInfoPage } from "../auth/UserInfoPage";
+import { ProductPage } from "../products/pages/ProductPage";
+import { ProductCatPage } from "../pages/ProductCatPage";
+import { useUsers } from "../context/store/UsersProvider";
+import { GlobalUserDataContext } from "../auth/helpers/globalUserData";
+import { ProductSearchPage } from "../pages/ProductSearchPage";
+import { Toaster } from "sonner";
 
 export const TiendaRouter = () => {
   const { getAnonToken, isLoading, userState } = useUsers();
@@ -25,20 +25,24 @@ export const TiendaRouter = () => {
 
   // console.log(userState.token.accessToken);
 
-  if (isLoading) return 'Cargando...';
+  if (isLoading) return "Cargando...";
   return (
     <>
       <ResponsiveAppBar />
+<<<<<<< HEAD
       <Toaster position='bottom-right' richColors />
+=======
+      <Toaster position="bottom-right" richColors />
+>>>>>>> eecb77d (update repo)
       <div>
         <Routes>
-          <Route element={<HomePage />} path='/' />
-          <Route element={<RegisterPage />} path='/auth/registro' />
-          <Route element={<UserInfoPage />} path='/auth/user' />
-          <Route element={<LogInPage />} path='/auth/login' />
-          <Route element={<ProductPage />} path='/instrumentos/:id' />
-          <Route element={<ProductCatPage />} path='/instrumentos/cat/:id' />
-          <Route element={<ProductSearchPage />} path='/search' />
+          <Route element={<HomePage />} path="/" />
+          <Route element={<RegisterPage />} path="/auth/registro" />
+          <Route element={<UserInfoPage />} path="/auth/user" />
+          <Route element={<LogInPage />} path="/auth/login" />
+          <Route element={<ProductPage />} path="/instrumentos/:id" />
+          <Route element={<ProductCatPage />} path="/instrumentos/cat/:id" />
+          <Route element={<ProductSearchPage />} path="/search" />
         </Routes>
       </div>
       <Footer />
